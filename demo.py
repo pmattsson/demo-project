65d452eee85ecb8981fe171b6fae58732e28d5ec
Sami Berndtson <75847598+samiberndtson@users.noreply.github.com>
2023-09-07 16:29:45 +0200

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,28 @@
+import sys
+import json
+import re
+import urllib.request
+import socket, ssl
+from itertools import zip_longest
+import argparse
+import http.client
+import json
+from urllib import parse
+import time
+import itertools
+
+
+
+
+
+def main():
+
+    i = 0
+
+    print("Hello World!")
+
+    sys.exit()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
